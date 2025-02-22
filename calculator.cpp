5407f615b8f7ca82f00e4220b7b16e880c31fd5e
Champ <champsrw49@gmail.com>
2025-02-22 10:34:32 +0700

    complete

--- calculator.cpp
+++ calculator.cpp
@@ -1,58 +1,275 @@
+#if defined(UNICODE) && !defined(_UNICODE)
+
+    #define _UNICODE
+
+#elif defined(_UNICODE) && !defined(UNICODE)
+
+    #define UNICODE
+
+#endif
+
+
+
+#include <tchar.h>
+
 #include <windows.h>
+
 #include <stdio.h>
 
-HWND hNum1, hNum2, hAdd, hSub, hMul, hDiv;
 
-LRESULT CALLBACK WndProc(HWND hwnd, UINT Message, WPARAM wParam, LPARAM lParam) {
-    char buffer[256];
-    double num1, num2, result;
-    switch (Message) {
-        case WM_CREATE:
-            CreateWindow("STATIC", "Please input two numbers", WS_VISIBLE | WS_CHILD, 20, 10, 200, 20, hwnd, NULL, NULL, NULL);
-            hNum1 = CreateWindow("EDIT", "", WS_VISIBLE | WS_CHILD | WS_BORDER | WS_TABSTOP, 20, 40, 200, 20, hwnd, (HMENU)5, NULL, NULL);
-            hNum2 = CreateWindow("EDIT", "", WS_VISIBLE | WS_CHILD | WS_BORDER | WS_TABSTOP , 20, 70, 200, 20, hwnd, (HMENU)6, NULL, NULL);
-            hAdd = CreateWindow("BUTTON", "+", WS_VISIBLE | WS_CHILD | WS_TABSTOP, 20, 100, 40, 40, hwnd, (HMENU)1, NULL, NULL);
-            hSub = CreateWindow("BUTTON", "-", WS_VISIBLE | WS_CHILD | WS_TABSTOP, 70, 100, 40, 40, hwnd, (HMENU)2, NULL, NULL);
-            hMul = CreateWindow("BUTTON", "*", WS_VISIBLE | WS_CHILD | WS_TABSTOP, 120, 100, 40, 40, hwnd, (HMENU)3, NULL, NULL);
-            hDiv = CreateWindow("BUTTON", "/", WS_VISIBLE | WS_CHILD | WS_TABSTOP, 170, 100, 40, 40, hwnd, (HMENU)4, NULL, NULL);
-            break;
-        case WM_COMMAND:
-            if (LOWORD(wParam) == 1 || LOWORD(wParam) == 2 || LOWORD(wParam) == 3 || LOWORD(wParam) == 4) {
-                GetWindowText(hNum1, buffer, 256);
-                num1 = atof(buffer);
-                GetWindowText(hNum2, buffer, 256);
-                num2 = atof(buffer);
-                switch (LOWORD(wParam)) {
-                    case 1: result = num1 + num2; break;
-                    case 2: result = num1 - num2; break;
-                    case 3: result = num1 * num2; break;
-                    case 4: result = (num2 != 0) ? num1 / num2 : 0; break;
-                }
-                sprintf(buffer, "%.2f", result);
-                MessageBox(hwnd, buffer, "Result", MB_OK);
-            }
-            break;
-        case WM_DESTROY:
-            PostQuitMessage(0);
-            break;
-        default:
-            return DefWindowProc(hwnd, Message, wParam, lParam);
+
+LRESULT CALLBACK WindowProcedure(HWND, UINT, WPARAM, LPARAM);
+
+
+
+HWND TextBox1, TextBox2;
+
+HWND button_plus, button_mi, button_time, button_divi;
+
+
+
+TCHAR szClassName[] = _T("CodeBlocksWindowsApp");
+
+
+
+int WINAPI WinMain(HINSTANCE hThisInstance,
+
+                   HINSTANCE hPrevInstance,
+
+                   LPSTR lpszArgument,
+
+                   int nCmdShow)
+
+{
+
+    HWND hwnd;
+
+    MSG messages;
+
+    WNDCLASSEX wincl;
+
+
+
+    wincl.hInstance = hThisInstance;
+
+    wincl.lpszClassName = szClassName;
+
+    wincl.lpfnWndProc = WindowProcedure;
+
+    wincl.style = CS_DBLCLKS;
+
+    wincl.cbSize = sizeof(WNDCLASSEX);
+
+    wincl.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+
+    wincl.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
+
+    wincl.hCursor = LoadCursor(NULL, IDC_ARROW);
+
+    wincl.lpszMenuName = NULL;
+
+    wincl.cbClsExtra = 0;
+
+    wincl.cbWndExtra = 0;
+
+    wincl.hbrBackground = (HBRUSH)COLOR_BACKGROUND;
+
+
+
+    if (!RegisterClassEx(&wincl))
+
+        return 0;
+
+
+
+    hwnd = CreateWindowEx(
+
+        0,
+
+        szClassName,
+
+        _T("My Calculator"),
+
+        WS_MINIMIZE | WS_SYSMENU,
+
+        CW_USEDEFAULT, CW_USEDEFAULT, 250, 200,
+
+        HWND_DESKTOP, NULL, hThisInstance, NULL);
+
+
+
+    ShowWindow(hwnd, nCmdShow);
+
+
+
+    while (GetMessage(&messages, NULL, 0, 0))
+
+    {
+
+        TranslateMessage(&messages);
+
+        DispatchMessage(&messages);
+
     }
-    return 0;
+
+
+
+    return messages.wParam;
+
 }
 
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
-    WNDCLASSEX wc = { sizeof(WNDCLASSEX), CS_HREDRAW | CS_VREDRAW, WndProc, 0, 0, hInstance, NULL, LoadCursor(NULL, IDC_ARROW), 
-                       (HBRUSH)COLOR_WINDOW + 1, NULL, "MyCalculator", NULL };
-    RegisterClassEx(&wc);
-    HWND hwnd = CreateWindowEx(0, "MyCalculator", "My Calculator", WS_OVERLAPPED | WS_SYSMENU, CW_USEDEFAULT, CW_USEDEFAULT, 250, 200, NULL, NULL, hInstance, NULL);
-    if (!hwnd) return 0;
-    ShowWindow(hwnd, nCmdShow);
-    UpdateWindow(hwnd);
-    MSG msg;
-    while (GetMessage(&msg, NULL, 0, 0)) {
-        TranslateMessage(&msg);
-        DispatchMessage(&msg);
+
+
+LRESULT CALLBACK WindowProcedure(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
+
+{
+
+    switch (message)
+
+    {
+
+    case WM_ERASEBKGND:
+
+    {
+
+        HDC hdc = (HDC)wParam;
+
+        RECT rc;
+
+        GetClientRect(hwnd, &rc);
+
+        HBRUSH hBrush = CreateSolidBrush(RGB(255, 51, 255));
+
+        FillRect(hdc, &rc, hBrush);
+
+        DeleteObject(hBrush);
+
+        return 1;
+
     }
-    return msg.wParam;
+
+
+
+    case WM_CREATE:
+
+        CreateWindow("STATIC", "Please input two numbers", WS_VISIBLE | WS_CHILD,
+
+                     20, 20, 200, 25, hwnd, NULL, NULL, NULL);
+
+
+
+        TextBox1 = CreateWindow("EDIT", "", WS_BORDER | WS_VISIBLE | WS_CHILD,
+
+                                20, 50, 200, 25, hwnd, (HMENU)101, NULL, NULL);
+
+        TextBox2 = CreateWindow("EDIT", "", WS_BORDER | WS_VISIBLE | WS_CHILD,
+
+                                20, 80, 200, 25, hwnd, (HMENU)102, NULL, NULL);
+
+
+
+        button_plus = CreateWindow("BUTTON", "+", WS_VISIBLE | WS_CHILD | WS_BORDER,
+
+                                   30, 120, 30, 30, hwnd, (HMENU)1, NULL, NULL);
+
+        button_mi = CreateWindow("BUTTON", "-", WS_VISIBLE | WS_CHILD | WS_BORDER,
+
+                                 70, 120, 30, 30, hwnd, (HMENU)2, NULL, NULL);
+
+        button_time = CreateWindow("BUTTON", "*", WS_VISIBLE | WS_CHILD | WS_BORDER,
+
+                                   110, 120, 30, 30, hwnd, (HMENU)3, NULL, NULL);
+
+        button_divi = CreateWindow("BUTTON", "/", WS_VISIBLE | WS_CHILD | WS_BORDER,
+
+                                   150, 120, 30, 30, hwnd, (HMENU)4, NULL, NULL);
+
+        break;
+
+
+
+    case WM_COMMAND:
+
+        if (HIWORD(wParam) == BN_CLICKED)
+
+        {
+
+            char num1[10], num2[10];
+
+            GetWindowText(TextBox1, num1, 10);
+
+            GetWindowText(TextBox2, num2, 10);
+
+
+
+            long double val1 = strtold(num1,NULL);
+
+            long double val2 = strtold(num2,NULL);
+
+            long double result = 0;
+
+            char resText[100];
+
+
+
+            switch (LOWORD(wParam))
+
+            {
+
+            case 1:
+
+                result = val1 + val2;
+
+                break;
+
+            case 2:
+
+                result = val1 - val2;
+
+                break;
+
+            case 3:
+
+                result = val1 * val2;
+
+                break;
+
+            case 4:
+
+                result = val1 / val2;
+
+                break;
+
+            }
+
+
+
+            snprintf(resText, sizeof(resText), "%.7Lf", result);
+
+            MessageBox(hwnd, resText, "Result", MB_OK );
+
+        }
+
+        break;
+
+
+
+    case WM_DESTROY:
+
+        PostQuitMessage(0);
+
+        break;
+
+
+
+    default:
+
+        return DefWindowProc(hwnd, message, wParam, lParam);
+
+    }
+
+
+
+    return 0;
+
 }
